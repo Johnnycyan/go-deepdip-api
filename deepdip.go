abe5ebc1d19af7bdd886d9dc8e33557950d66dbe
John <24556317+Johnnycyan@users.noreply.github.com>
2024-05-08 07:11:24 -0400

    Inital Commit

---
+++ deepdip.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"net/http"
+	"os"
+	"strconv"
+
+	tmio "github.com/Johnnycyan/go-tmio-sdk"
+)
+
+type DDPlayer struct {
+	Height      float64 `json:"height"`
+	Name        string  `json:"name"`
+	Rank        int     `json:"rank"`
+	TS          int     `json:"ts"`
+	UpdateCount int     `json:"update_count"`
+	WSID        string  `json:"wsid"`
+}
+
+type Leaderboard []DDPlayer
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Usage: deepdip <port>")
+		return
+	}
+	port := args[0]
+	fmt.Println("Listening on http://localhost:" + port)
+	http.HandleFunc("/pb", getPB)
+	http.HandleFunc("/leaderboards", getLeaderboards)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func deepDipAPIPlayer(playerID string) (*DDPlayer, error) {
+	url := fmt.Sprintf("https://dips-plus-plus.xk.io/leaderboard/%s", playerID)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var player DDPlayer
+	if err := json.NewDecoder(resp.Body).Decode(&player); err != nil {
+		return nil, err
+	}
+	return &player, nil
+}
+
+func deepDipAPILeaderboard() (*Leaderboard, error) {
+	url := "https://dips-plus-plus.xk.io/leaderboard/global"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var leaderboard Leaderboard
+	if err := json.NewDecoder(resp.Body).Decode(&leaderboard); err != nil {
+		return nil, err
+	}
+	return &leaderboard, nil
+}
+
+func getPB(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		http.Error(w, "username is required", http.StatusBadRequest)
+		return
+	}
+	playerID, err := tmio.GetPlayerID(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	player, err := deepDipAPIPlayer(playerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	roundedHeight := int(math.Round(player.Height))
+	fmt.Fprint(w, player.Name+" is rank #"+strconv.Itoa(player.Rank)+" ("+strconv.Itoa(roundedHeight)+"m)")
+}
+
+func getLeaderboards(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	var usernameExists bool
+	if username == "" {
+		usernameExists = false
+	} else {
+		usernameExists = true
+	}
+
+	leaderboard, err := deepDipAPILeaderboard()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var medal string
+	var playersString string
+	for i, player := range *leaderboard {
+		if i >= 3 {
+			break
+		} else if i == 0 {
+			medal = "🥇 "
+		} else if i == 1 {
+			medal = "🥈 "
+		} else if i == 2 {
+			medal = "🥉 "
+		} else {
+			medal = ""
+		}
+		roundedHeight := int(math.Round(player.Height))
+		playersString += medal + player.Name + " (" + strconv.Itoa(roundedHeight) + "m) "
+	}
+
+	playerID, err := tmio.GetPlayerID(username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	player, err := deepDipAPIPlayer(playerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var userString string
+	if usernameExists {
+		roundedHeight := int(math.Round(player.Height))
+		userString = "| " + player.Name + "'s PB is rank #" + strconv.Itoa(player.Rank) + " with a height of (" + strconv.Itoa(roundedHeight) + "m)"
+	} else {
+		userString = ""
+	}
+
+	fullstring := "Current standing: " + playersString + userString
+	fmt.Fprint(w, fullstring)
+}
